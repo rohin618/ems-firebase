28c97d2960b75534ffd8e1b9f80d4d0e15974176
rohin618 <rohinikumar.deceswc1@email.com>
2025-09-22 09:16:00 +0530

    changes

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,17 @@
-import React from "react";
-import { StrictMode } from "react";
+import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter, HashRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContest";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-  <HashRouter>
-   <AuthProvider>
-     <App />
-   </AuthProvider>
-  </HashRouter>
+    <HashRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </HashRouter>
   </StrictMode>
 );
+
