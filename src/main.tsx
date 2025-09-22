9bc36ef416d31e9813ed5fe9fcab82e5abaa43d0
rohin618 <rohinikumar.deceswc1@email.com>
2025-09-22 09:02:31 +0530

    changeto hash

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContest";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-  <BrowserRouter>
+  <HashRouter>
    <AuthProvider>
      <App />
    </AuthProvider>
-  </BrowserRouter>
+  </HashRouter>
   </StrictMode>
 );
