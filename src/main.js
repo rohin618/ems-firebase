5a87bfb6b27133a3bf15fa8444da5bab1f41a55c
rohin618 <rohinikumar.deceswc1@email.com>
2025-09-22 09:28:28 +0530

    last commit

--- src/main.js
+++ src/main.js
@@ -2,9 +2,9 @@ import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContest";
 createRoot(document.getElementById("root")).render(React.createElement(StrictMode, null,
-    React.createElement(HashRouter, null,
+    React.createElement(BrowserRouter, null,
         React.createElement(AuthProvider, null,
             React.createElement(App, null)))));
--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,16 @@ import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContest";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <AuthProvider>
         <App />
       </AuthProvider>
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>
 );
 
