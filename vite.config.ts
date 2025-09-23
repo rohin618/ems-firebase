2e4e25b15f46baab7dee922428304b6be6764783
rohin618 <rohinikumar.deceswc1@email.com>
2025-09-23 16:51:54 +0530

    update the config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/ems-firebase/', // MUST match your repo name
+  base: 'ems-firebase/', // MUST match your repo name
   plugins: [react()],
 });
